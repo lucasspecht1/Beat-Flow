9437c6c059ea9eceae5c046438eb7dd2c0756f14
lucasspecht1 <lucasspecht4@gmail.com>
2024-01-30 02:06:52 -0300

    Preparação Login

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar";
+
+export default function Login() {
+    return (
+        <>
+            
+            <div className="w-full h-screen flex justify-center items-center">
+                <div className="border-2 border-[#0f113038] bg-[#0a0a2410] w-[80%] h-[80%] rounded-3xl flex justify-end">
+                    <div className=" rounded-3xl h-full w-[65%]">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    ); 
+}
